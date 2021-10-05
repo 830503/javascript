37fcec3e6700d97db56616b587d8cd0d99c4ba37
830503 <lixiaoqi19830503@gmail.com>
2021-10-05 14:57:17 +0200

    finish 03 in fetch

--- js/js-complete-course/07-fetch/03-list-to-template-Optional/script.js
+++ js/js-complete-course/07-fetch/03-list-to-template-Optional/script.js
@@ -10,5 +10,29 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+
+
+document.getElementById('run').addEventListener('click', function(){
+    let target = document.getElementById('target');
+    async function getAPI(){
+        let response = await fetch('http://localhost:3000/heroes');
+        let data = await response.json();
+        
+        for(i = 0; i < data.length; i++){
+            let temp = document.getElementById('tpl-hero').content.cloneNode(true);
+
+            temp.querySelector('.name').innerHTML = data[i].name;
+            temp.querySelector('em.alter-ego').innerHTML = data[i].alterEgo;
+            temp.querySelector('.powers').innerHTML = data[i].abilities;
+            target.appendChild(temp);
+        }
+            
+        
+        
+    }
+
+getAPI();
+
+})
+
 })();
