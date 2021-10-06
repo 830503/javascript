963c3fc14090413f5b0d03a8c9426240e1e9cf41
830503 <lixiaoqi19830503@gmail.com>
2021-10-06 11:06:20 +0200

    finish 04 in fetch

--- js/js-complete-course/07-fetch/04-add-Optional/script.js
+++ js/js-complete-course/07-fetch/04-add-Optional/script.js
@@ -10,5 +10,29 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    
+    document.getElementById('run').addEventListener('click', function(){
+        async function getAPI(){
+            var response = await fetch('http://localhost:3000/heroes');
+            var data = await response.json();
+    
+            let names = document.getElementById('hero-name').value;
+            let alter_Ego = document.getElementById('hero-alter-ego').value;
+            let power = document.getElementById('hero-powers').value;
+    
+            let newData = {
+                id: data.length +  1,
+                name: names,
+                alterEgo: alter_Ego,
+                abilities: power
+            }
+            data.push(newData);
+            console.log(data);
+        }
+      
+        getAPI();
+    })
+
+
+
 })();
