6b2d931f3ac82b33ae4af332b57843ca7bbdbe74
830503 <lixiaoqi19830503@gmail.com>
2021-10-08 10:28:16 +0200

    finish 04 in misc

--- js/js-complete-course/optional/01-misc/04-worst-ui-two/script.js
+++ js/js-complete-course/optional/01-misc/04-worst-ui-two/script.js
@@ -10,7 +10,29 @@
 // You will have time to focus on it later.
 
 (function() {
+    let btn1 = document.getElementById('part-one');
+    let btn2 = document.getElementById('part-two');
+    let btn3 = document.getElementById('part-three');
+    let btn4 = document.getElementById('part-four');
+    let target = document.getElementById('target');
+    let buttons = [btn1, btn2, btn3, btn4];
 
-    // your code here
+    buttons.forEach(myFunction);
+    function myFunction(button){
+        button.addEventListener('click', function(){
+            let min = button.getAttribute('data-min');
+            let max = button.getAttribute('data-max');
+            let input = parseInt(button.innerHTML) + 1;
+            if(input > max){
+                input = min;
+            }else if (input < 10){
+                button.innerHTML = '0' + input;
+            } else {
+                button.innerHTML = input.toString();
+            }
+            target.innerHTML = '+' + btn1.innerText + btn2.innerText + btn3.innerText + btn4.innerText;
+        })
+
+    }
 
 })();
